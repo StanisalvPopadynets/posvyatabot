33d70ec3cd2aabc6ce921f83ed51789fc5ba46cb
Stanislav Popadynets <popadynetss@gmail.com>
2021-09-07 23:01:14 +0300

    Bot setup

---
+++ index.js
@@ -0,0 +1,11 @@
+const TelegramBot = require("node-telegram-bot-api");
+require("dotenv").config();
+
+const token = process.env.TELEGRAM_BOT_TOKEN;
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on("message", (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, "Received");
+});
