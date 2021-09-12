58433d3c9fa770eb74ebe17857457faaefffd0d4
Stanislav Popadynets <popadynetss@gmail.com>
2021-09-12 13:23:52 +0300

    Add two commands and message consts

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 import TelegramBot from "node-telegram-bot-api";
+import { formatInfoMsg, greetingMsg, invalidLinksMsg, notALinkMsg, validLinkMsg } from "./constants.js";
 import { validateLink } from "./utils.js";
 
 const token = process.env.TELEGRAM_BOT_TOKEN;
 
 const bot = new TelegramBot(token, { polling: true });
 
-bot.on("message", (msg) => {
-  const chatId = msg.chat.id;
-  let responseMessage = "INVALID";
-  if (validateLink(msg.text)) {
-    responseMessage = "VALID";
+bot.on("message", ({ text, chat: { id } }) => {
+  if (text[0] === "/") return;
+  let responseMessage = notALinkMsg;
+  if (validateLink(text)) {
+    responseMessage = validLinkMsg;
+  } else {
+    if (/http/.test(text)) {
+      responseMessage = invalidLinksMsg;
+    }
   }
 
-  bot.sendMessage(chatId, responseMessage);
+  bot.sendMessage(id, responseMessage);
+});
+
+bot.onText(/\/start/, ({ chat: { id } }) => {
+  bot.sendMessage(id, greetingMsg);
+});
+
+bot.onText(/\/format_info/, ({ chat: { id } }) => {
+  bot.sendMessage(id, formatInfoMsg);
 });
 
 bot.on("polling_error", console.log);
