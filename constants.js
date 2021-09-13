d70746bae3f0c34ebd8e125b16bfc3b8b0e5e07b
Stanislav Popadynets <popadynetss@gmail.com>
2021-09-14 00:47:18 +0300

    Correct soundcloud regexp and handle not a text message

--- constants.js
+++ constants.js
@@ -2,7 +2,7 @@ export const YOUTUBE_REGEXP =
   /(?:http?s?:\/\/)?(?:www.)?(?:m.)?(?:music.)?youtu(?:\.?be)(?:\.com)?(?:(?:\w*.?:\/\/)?\w*.?\w*-?.?\w*\/(?:embed|e|v|watch|.*\/)?\??(?:feature=\w*\.?\w*)?&?(?:v=)?\/?)([\w\d_-]{11})(?:\S+)?/gm;
 export const YOUTUBE_SAFEGUARD_REGEXP =
   /(?:https?:\/\/)?(?:www\.)?youtu\.?be(?:\.com)?\/?.*(?:watch|embed)?(?:.*v=|v\/|\/)([\w\-_]+)\&?/gm;
-export const SOUNDCLOUD_REGEXP = /^(?:(https?):\/\/)?(?:(?:www|m)\.)?(soundcloud\.com|snd\.sc)\/(.*)$/gm;
+export const SOUNDCLOUD_REGEXP = /^https?:\/\/(www\.)?(soundcloud)\.(app|com).*$/;
 export const SPOTIFY_REGEXP = /^(?:https:\/\/open\.spotify\.com|spotify)\/(track|playlist)/gm;
 
 export const greetingMsg =
